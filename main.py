6d4c595bf5bbefe639412521ddf13be33b9b5576
Javier Lozano <fj.lozano.11@gmail.com>
2026-02-07 20:40:30 -0600

    Conexión a Streamlit

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("Hello World")
+import streamlit as st
+
+st.set_page_config(page_title="FantasyPL Chatbot", layout="wide")
+st.title("FantasyPL Chatbot 🤖 ⚽")
+
+st.write("Si ves esto, Streamlit está corriendo.")
